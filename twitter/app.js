2bc33b1a65457c413bc24cc010af314c5792fb36
Nicolás Navarrete <68087904+nnavarrete-mcr@users.noreply.github.com>
2021-04-06 13:57:26 -0500

    docs: Add comments to lambda function

--- twitter/app.js
+++ twitter/app.js
@@ -1,29 +1,48 @@
+// Global vars
 let response;
 const axios = require('axios')
 
 exports.lambdaHandler = async (event, context) => {
     
+    // Vars
     let username = event.queryStringParameters.username;
     let userId = null;
     let result = '';
 
+    /*
+    * Call the async function to get the username id from twetter
+    */
     let dataUserName = await retriveDataFromAPI(`/users/by/username/${username}`);
-    
+
+    /*
+    * If the result has an error, then return 
+    * in messages with the detailed error
+    * If the result doesn't have an error, get the user id
+    */
     if(dataUserName.error == false){
         userId = dataUserName.result.id;
+
+        /*
+        * Call the async function to get the tweets form the user id
+        */
         let dataTweets = await retriveDataFromAPI(`/users/${userId}/tweets`);
         
+        // Object with the tweets results
         result = {
             error: false,
             result: dataTweets.result
         }
     } else {
+        // Object with the error
         result = {
             error: true,
             messageError: dataUserName.messageError
         }
     }
     
+    /*
+    * Retrun the response with the result
+    */
     return response = {
         'statusCode': 200,
         'body': JSON.stringify(result)
@@ -32,6 +51,7 @@ exports.lambdaHandler = async (event, context) => {
 
 
 async function retriveDataFromAPI(url){
+    // vars
     let error = false;
     let messageError = "";
     let result = "";
@@ -39,7 +59,12 @@ async function retriveDataFromAPI(url){
     const bearerToken = process.env.twitterBearerToken;
     const twitterEndPoint = process.env.twitterEndPoint;
 
+    // Try catch statement
     try {
+        /*
+        * Use of the axio to get the Twetter API Response
+        * Autorization Bearer in the header
+        */
         response = await axios.get(
             twitterEndPoint + url,
             {
@@ -47,6 +72,10 @@ async function retriveDataFromAPI(url){
             }
         );
         
+        /*
+        * If the response has error
+        * all errors are captured with a loop
+        */
         if(response.data.errors){
             response.data.errors.forEach(element => {
                 errorsDetailed.push(element.detail);
@@ -54,14 +83,21 @@ async function retriveDataFromAPI(url){
             error = true;
             messageError = errorsDetailed;
         } else {
+
+            // The data is saved in the result var
             result = response.data.data;
         }
     } catch (error) {
-        console.log(error)
+        /*
+        * Handle the error
+        */
         error = true;
         messageError = "Internal Server Error";
     }
 
+    /*
+    * Retrun the result 
+    */
     return {
         error: error,
         messageError: messageError,
